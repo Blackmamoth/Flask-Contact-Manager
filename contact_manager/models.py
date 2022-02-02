86409d464aa7bd009fc6ba0cc3b87bb287bea2cf
Ashpak Veetar <76839201+Blackmamoth@users.noreply.github.com>
2022-02-02 23:42:12 +0530

    Little update to models

--- contact_manager/models.py
+++ contact_manager/models.py
@@ -8,7 +8,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
+    username = db.Column(db.String(40), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     contacts = db.relationship('Contact', backref='contact_saver', lazy=True)
@@ -18,7 +18,7 @@ class User(db.Model, UserMixin):
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
